fe08d99d2e27a24100bede21f6b7e09991c89976
Tyler Campbell <thehaqua@gmail.com>
2021-10-07 18:51:39 -0400

    commit 2 - style update

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 const makePost = require('./makePost');
+const thread = require('./thread');
+const catalog = require('./catalog');
 //const projectRoutes = require('./projectRoutes');
 
 router.use('/post', makePost);
+router.use('/thread', thread);
+router.use('/catalog', catalog);
 //router.use('/projects', projectRoutes);
 
 module.exports = router;
---
+++ controllers/api/thread.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    try {
+        res.render('thread');
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
